3995ff46317374601e3a9cbaa4f85b61e8e8ae93
calebcartwright <caleb.cartwright@outlook.com>
2019-03-17 19:50:38 -0500

    feat(node): finished combo templates

--- test/yml/node/combo/npm_test_lint_validation_xunit_cobertura_test.py
+++ test/yml/node/combo/npm_test_lint_validation_xunit_cobertura_test.py
@@ -1,20 +1,23 @@
 from test.test_utilities import parse_node_combo_template_yaml_file
 
-fileName = "npm-build-validation-xunit-cobertura.yml"
+fileName = "npm-test-lint-validation-xunit-cobertura.yml"
 contents = parse_node_combo_template_yaml_file(fileName)
 steps = contents["steps"]
 parameters = contents["parameters"]
 npmInstallParameters = parameters["npmInstall"]
-runNpmBuildScriptParameters = parameters["runNpmBuildScript"]
+npmTestScriptParameters = parameters["npmTestScript"]
+npmLintScriptParameters = parameters["npmLintScript"]
 publishTestResultsParameters = parameters["publishTestResults"]
 publishCoverageResultsParameters = parameters["publishCoverage"]
 npmInstallStep = steps[0]
 npmInstallStepParameters = npmInstallStep["parameters"]
-runNpmBuildScriptStep = steps[1]
-runNpmBuildScriptStepParameters = runNpmBuildScriptStep["parameters"]
-publishTestResultsStep = steps[2]
+npmTestScriptStep = steps[1]
+npmTestScriptStepParameters = npmTestScriptStep["parameters"]
+npmLintScriptStep = steps[2]
+npmLintScriptStepParameters = npmLintScriptStep["parameters"]
+publishTestResultsStep = steps[3]
 publishTestResultsStepParameters = publishTestResultsStep["parameters"]
-publishCodeCoverageStep = steps[3]
+publishCodeCoverageStep = steps[4]
 publishCodeCoverageStepParameters = publishCodeCoverageStep["parameters"]
 
 
@@ -22,12 +25,20 @@ def test_npm_install_step_display_name_parameter_default():
     assert npmInstallParameters["taskDisplayName"] == "Install dependencies"
 
 
-def test_run_npm_build_script_step_script_name_parameter_default():
-    assert runNpmBuildScriptParameters["buildNpmScriptName"] == "build"
+def test_npm_test_script_step_script_name_name_parameter_default():
+    assert npmTestScriptParameters["npmTestScriptName"] == "test"
 
 
-def test_run_npm_build_script_step_display_name_parameter_default():
-    assert runNpmBuildScriptParameters["taskDisplayName"] == "Run build script"
+def test_npm_test_script_step_display_name_parameter_default():
+    assert npmTestScriptParameters["taskDisplayName"] == "Run tests"
+
+
+def test_npm_lint_script_step_script_name_parameter_default():
+    assert npmLintScriptParameters["npmLintScriptName"] == "lint"
+
+
+def test_npm_lint_script_step_display_name_parameter_default():
+    assert npmLintScriptParameters["taskDisplayName"] == "Lint"
 
 
 def test_publish_test_results_step_results_format_parameter_default():
@@ -83,7 +94,7 @@ def test_publish_code_coverage_step_task_display_name_parameter_default():
 
 
 def test_num_steps():
-    assert len(steps) == 4
+    assert len(steps) == 5
 
 
 def test_npm_install_step_template_path():
@@ -91,23 +102,38 @@ def test_npm_install_step_template_path():
 
 
 def test_npm_install_step_display_name_parameter():
-    value = npmInstallParameters["taskDisplayName"]
+    value = npmInstallStepParameters["taskDisplayName"]
     assert value == "${{ parameters.npmInstall.taskDisplayName }}"
 
 
-def test_npm_run_build_script_step_template_path():
-    value = runNpmBuildScriptStep["template"]
-    assert value == "../steps/simple/npm-run-build.yml"
+def test_npm_test_script_step_template_path():
+    value = npmTestScriptStep["template"]
+    assert value == "../steps/simple/npm-run-test.yml"
 
 
-def test_npm_run_build_script_build_script_name_parameter():
-    value = runNpmBuildScriptStepParameters["buildNpmScriptName"]
-    assert value == "${{ parameters.runNpmBuildScript.buildNpmScriptName }}"
+def test_npm_test_script_step_script_name_parameter():
+    value = npmTestScriptStepParameters["testNpmScriptName"]
+    assert value == "${{ parameters.npmTestScript.npmTestScriptName }}"
 
 
-def test_npm_install_step_display_name_parameter():
-    value = runNpmBuildScriptStepParameters["taskDisplayName"]
-    assert value == "${{ parameters.runNpmBuildScript.taskDisplayName }}"
+def test_npm_test_step_display_name_parameter():
+    value = npmTestScriptStepParameters["taskDisplayName"]
+    assert value == "${{ parameters.npmTestScript.taskDisplayName }}"
+
+
+def test_npm_lint_script_step_template_path():
+    value = npmLintScriptStep["template"]
+    assert value == "../steps/simple/npm-run-lint.yml"
+
+
+def test_npm_lint_script_step_script_name_parameter():
+    value = npmLintScriptStepParameters["lintNpmScriptName"]
+    assert value == "${{ parameters.npmLintScript.npmLintScriptName }}"
+
+
+def test_npm_lint_step_display_name_parameter():
+    value = npmLintScriptStepParameters["taskDisplayName"]
+    assert value == "${{ parameters.npmLintScript.taskDisplayName }}"
 
 
 def test_publish_step_template_path():
