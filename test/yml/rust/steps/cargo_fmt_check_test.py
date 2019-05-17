c03591c8482a4d5e661506b28895354ea4041ac8
calebcartwright <caleb.cartwright@outlook.com>
2019-05-16 20:28:10 -0500

    feat(rust): finished rustfmt check step template

---
+++ test/yml/rust/steps/cargo_fmt_check_test.py
@@ -0,0 +1,22 @@
+from test.test_utilities import parse_rust_step_template_yaml_file
+
+contents = parse_rust_step_template_yaml_file("cargo-fmt-check.yml")
+parameters = contents["parameters"]
+steps = contents["steps"]
+step = steps[0]
+
+
+def test_display_name_parameter_default():
+    assert parameters["taskDisplayName"] == "rustfmt check"
+
+
+def test_num_steps():
+    assert len(steps) == 1
+
+
+def test_script_contents():
+    assert step["script"] == "cargo fmt -- --check"
+
+
+def test_script_display_name():
+    assert step["displayName"] == "${{ parameters.taskDisplayName }}"
